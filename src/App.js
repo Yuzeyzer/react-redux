87927f830c19290c4ba6313d0a5c1220cdd7417a
John Doe <kurbanof.atabek@gmail.com>
2022-02-27 03:29:37 +0600

    created ApprovalCard and nestem CommenDetails inside of it

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import CommentDetail from './components/CommentDetail';
 import faker from '@faker-js/faker';
+import ApprovalCard from './components/ApprovalCard';
 
 export default function App() {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Сергей'
-        timeAgo='20 часов назад'
-        content='Отличный пост, много полезного!'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Марлен'
-        timeAgo='17 часов назад'
-        content='Да нее, одна вода да и только'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Кайратик'
-        timeAgo='7 часов назад'
-        content='Да норм, че наехали на девушку?'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Ислам'
-        timeAgo='4 часов назад'
-        content='Это парень...'
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Сергей'
+          timeAgo='20 часов назад'
+          content='Отличный пост, много полезного!'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Марлен'
+          timeAgo='17 часов назад'
+          content='Да нее, одна вода да и только'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Кайратик'
+          timeAgo='7 часов назад'
+          content='Да норм, че наехали на девушку?'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Ислам'
+          timeAgo='4 часов назад'
+          content='Это парень...'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
