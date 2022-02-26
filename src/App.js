9139bdeef18d35319c7d820ee068907c86979f9d
John Doe <kurbanof.atabek@gmail.com>
2022-02-27 01:30:12 +0600

    Components imports, add 3rd party libraries, Component Setups

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Field from './components/field';
+import Languages from './components/languages'
+import Translate from './components/translate'
 
 import './style.css'
 
 export default function App() {
   return (
-    <div>
-      <h1 className="title">Привет Чунбурунчупмеки!</h1>
+    <div className="container">
+      <Field />
+      <Languages />
+      <Translate />
     </div>
   );
 }
