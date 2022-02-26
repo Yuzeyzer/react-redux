c70d0eff7746f47c8ff32bdff0ca34e2cc7b6a62
John Doe <kurbanof.atabek@gmail.com>
2022-02-27 02:40:28 +0600

    installed FakerJS and added fake avatar image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import faker from '@faker-js/faker'
 
 export default function App() {
   return (
     <div className='ui container comments'>
       <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="avatar" />
+          <img src={faker.image.avatar()} alt="avatar" />
         </a>
         <div className="content">
           <a href="/" className="author">
