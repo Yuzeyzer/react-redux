ac230f3394be88a4ac40cdea49280b24d18501a6
John Doe <kurbanof.atabek@gmail.com>
2022-02-27 01:38:09 +0600

    useState basic hook, React props, translation is working now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 import Field from './components/field';
-import Languages from './components/languages'
-import Translate from './components/translate'
+import Languages from './components/languages';
+import Translate from './components/translate';
 
-import './style.css'
+import './style.css';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [language, setLanguage] = useState('ru');
+
   return (
-    <div className="container">
-      <Field />
-      <Languages />
-      <Translate />
+    <div className='container'>
+      <Field label='Enter english' onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
