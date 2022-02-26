9c9570a498717ae8ed6b8eed1d50115781cb87ae
John Doe <kurbanof.atabek@gmail.com>
2022-02-27 02:34:21 +0600

    comment JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-import './style.css'
-
 export default function App() {
   return (
-    <div>
-      
+    <div className='ui container comments'>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Сергей
+          </a>
+          <div className="metadata">
+            <span className="date">
+              Сегодня в 6 утра
+            </span>
+          </div>
+          <div className="text">
+            Хороший пост, Сэр!
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
