5b66f992b0ea4c86b21b68bbdc8659f2d22e3f70
John Doe <kurbanof.atabek@gmail.com>
2022-03-06 23:01:54 +0600

    installed axios, created funcion handleSearch with GET request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
+	const [term,setTerm] = useState('programming');
+
+	const handleSearch = async () => {
+		const response = await axios.get('https://en.wikipedia.org/w/api.php',{
+			params: {
+				action: 'query',
+				list: 'search',
+				origin: '*',
+				format: 'json',
+				srsearch: term
+			}
+		})
+		console.log(response)
+	};
+
+	useEffect(() => {
+		handleSearch()
+	},[term])
+
   return (
     <div>
       <div className='ui form'>
