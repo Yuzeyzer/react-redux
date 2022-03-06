82a0798ae72dcdad6fce49d3c793162fe0cd431d
John Doe <kurbanof.atabek@gmail.com>
2022-03-06 21:53:26 +0600

    added Accordion component with some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    id: 1,
+    title: 'Я люблю React',
+    content: 'Моя любовь к React безгранична и бездонна',
+  },
+  {
+    id: 2,
+    title: 'Чтобы сегодня приготовить',
+    content: 'Каждый вечер канибалиста - Сергея беспокоят томные мысли о еде',
+  },
+];
 
 export default function App() {
   return (
     <div className=''>
-     
+      <Accordion items={items}/>
     </div>
   );
 }
