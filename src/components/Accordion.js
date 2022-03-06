79267d3c2df4348de2a18c8d9f50a991fc458c41
John Doe <kurbanof.atabek@gmail.com>
2022-03-06 22:11:47 +0600

    add active classes based on item index using useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const renderedItems = items.map((item, index) => {
+    const active = activeIndex === index ? 'active' : '';
+
     return (
       <React.Fragment key={item.id}>
-        <div className='title active'>
+        <div onClick={() => setActiveIndex(index)} className={`title ${active}`}>
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='content active'>
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
