7c79707a24e3ab6d282e387244e0f3a537b65dc7
John Doe <kurbanof.atabek@gmail.com>
2022-03-27 21:10:36 +0600

    created State inside Convert and rendered data from API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const Convert = ({ language, text }) => {
-  useEffect(() => {
-    axios.post(
+  const [translated, setTranslated] = useState();
+
+  const getTranslatedWord = async () => {
+    const {
+      data: { data },
+    } = await axios.post(
       'https://translation.googleapis.com/language/translate/v2',
       {},
       {
@@ -14,9 +19,17 @@ const Convert = ({ language, text }) => {
         },
       },
     );
+    setTranslated(data.translations[0].translatedText);
+  };
+  useEffect(() => {
+    getTranslatedWord();
   }, [language, text]);
 
-  return <div>Convert</div>;
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
