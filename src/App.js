36750ace292687493a4a48d139c25e383576fd6a
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-06 16:36:00 +0600

    installed reac-router-dom and created 4 routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
