8aa7cc7c993f04825af7c1c4416bc8168d12ec25
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-08 04:23:10 +0600

    created RecipeList and used it in Home

---
+++ src/components/RecipeList/RecipeList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const RecipeList = ({ recipes }) => {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime}</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Приготовить </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
