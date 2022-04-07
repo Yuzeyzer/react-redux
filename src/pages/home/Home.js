fa41ca466d3cf75d668b0f47c33e1f074721098b
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-08 04:13:47 +0600

    fetching data from db.json and rendering in homeJS

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import "./style.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
 
-export default Home
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data &&
+        data.map((recipe) => {
+          return <h2 key={recipe.id}>{recipe.title}</h2>;
+        })}
+    </div>
+  );
+};
+
+export default Home;
