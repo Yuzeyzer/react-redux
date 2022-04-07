284cab7a6f4322ad0c54dde9de89d374c9636773
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-08 04:58:17 +0600

    Finished SearchJS component

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import RecipeList from '../../components/RecipeList/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
 import './style.css'
 
 const Search = () => {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get('q');
+
+  const url = `http://localhost:3000/recipes?q=${query}`;
+
+  const { data, isPending, error } = useFetch(url);
+
   return (
-    <div>Search</div>
+    <div>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
   )
 }
 
